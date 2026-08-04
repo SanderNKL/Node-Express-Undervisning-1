486e788fcb87babb015573424f8e59ccecb6ebbb
SanderNKL <sander.lastad@gmail.com>
2026-08-04 11:07:54 +0200

    removed validation from createUser and remove password form output

--- src/controllers/createUser.js
+++ src/controllers/createUser.js
@@ -1,28 +1,21 @@
 import { users } from "../store/users.store.js";
-import { validateUserDetails } from "../utils/validateUserDetails.js";
 
 
 export const createUser = (req, res) => {
     const { username, password, email } = req.body;
 
-    /* VALIDERING!! */
-    const validation = validateUserDetails({ username, password, email })
-    if (!validation.valid) {
-        return res.status(400).json({
-            error: validation.error
-        })
-    }
-
     /* Lage brukeren */
     const user = {
         id: 0,
         username,
-        email
+        email,
+        password
     }
 
     users.push(user)
 
-    res.json({
-        data: user
-    })
+    // delete password
+    delete user.password
+
+    res.json({ data: user })
 }
